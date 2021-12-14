34262082a117d7e4ac99f1d3c745bfc57e31f4ec
NicoleAnginoli <nicole_anginoli@hotmail.com>
2021-12-13 22:35:39 -0300

    feat: service para salvar os dados do usuário

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Person } from '../interfaces/Models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users: Person[] = [];
+  private storage: Storage;
+
+  constructor() {
+    this.storage = window.localStorage;
+  }
+
+  addUser(p: Person) {
+    // this.users.push(p);
+    this.set(p.username, p);
+  }
+
+  getUser(user: string) {
+    // return this.users.find(i => i.username === user)
+    return this.get(user);
+  }
+
+  set(key: string, value: any): boolean {
+    if (this.storage) {
+      this.storage.setItem(key, JSON.stringify(value));
+      return true;
+    }
+    return false;
+  }
+
+  get(key: string): any {
+    if (this.storage) {
+      let element = this.storage.getItem(key);
+      if (element)
+        return JSON.parse(element);
+      else
+        return null;
+    }
+    return null;
+  }
+}
