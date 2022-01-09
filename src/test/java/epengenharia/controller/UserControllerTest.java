6e7baa992378f82f5771f987fa38d1baa5f1bb64
NicoleAnginoli <nicole_anginoli@hotmail.com>
2022-01-09 20:18:20 -0300

    test: add testes projeto

--- src/test/java/epengenharia/controller/UserControllerTest.java
+++ src/test/java/epengenharia/controller/UserControllerTest.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.Rule;
-//import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
@@ -31,71 +31,92 @@ import org.springframework.test.context.junit4.SpringRunner;
 import epengenharia.model.User;
 import epengenharia.repository.UserRepository;
 
-@Disabled
 @RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 public class UserControllerTest {
-	
+
 	@Rule
-    public ExpectedException thrown = ExpectedException.none();
+	public ExpectedException thrown = ExpectedException.none();
 
 	@MockBean
 	UserRepository userRepository;
-	
+
 	@InjectMocks
 	private UserController userController;
-	
+
 	@Mock
 	private User user;
-	
+
+	private List<User> userList;
+
 	LocalDateTime time = LocalDateTime.of(2020, Month.MARCH, 26, 10, 31, 12, 761000000);
 
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.initMocks(this);
-//		this.funcList = new ArrayList<>();
-//		User teste = new User();
-//		
-//		this.funcList.add(usr1);
-//		this.funcList.add(usr2);
-//		this.funcList.add(usr3);
+		this.userList = new ArrayList<>();
+
+		this.userList.add(user);
+		this.userList.add(user);
+		this.userList.add(user);
 	}
 
-//	@Test
-//	void shouldReturnAllEmployees() throws Exception {
-//
-//		Mockito.when(userRepository.findAll()).thenReturn(funcList);
-//
-//		List<User> result = userController.getAllUsers();
-//		assertEquals(3, result.size());
-//	}
-//
-//	@Test
-//	void shouldReturnOneEmployeeById() {
-//		User func = new User(4, "nome", "11111111111", "e@email.com", time);
-//		Optional<User> funcO= Optional.of(func);
-//		Mockito.when(userRepository.findById(func.getId())).thenReturn(funcO);
-//		int id = 4;
-//		Optional<User> result = userController.getUserById(id);
-//		assertTrue(result.isPresent());
-//		assertThat(func.getId(), is(result.get().getId()));
-//	}
+	@Test
+	void shouldGetAllUsers() throws Exception {
+
+		Mockito.when(userRepository.findAll()).thenReturn(userList);
+
+		List<User> result = userController.getAllUsers();
+		assertEquals(3, result.size());
+	}
 
 	@Test
-	void shouldInsertNewEmployee() throws Exception {
+	void shouldGetUserByUsername() {
+		User usr1 = new User();
+		usr1.setUsername("user1");
+		Optional<User> user = Optional.of(usr1);
+
+		Mockito.when(userRepository.findByUsername(usr1.getUsername())).thenReturn(user);
+
+		String username = "user1";
+		Optional<User> result = userController.getUserByUsername(username);
+
+		assertTrue(result.isPresent());
+		assertThat(usr1.getUsername(), is(result.get().getUsername()));
+	}
+
+	@Test
+	void shouldSuccessfulyRegisterUser() throws Exception {
+		Mockito.when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.ofNullable(null));
 		Mockito.when(userRepository.save(user)).thenReturn(user);
 		ResponseEntity<User> result = userController.registerUser(user);
 		assertNotNull(result);
 	}
 
-//	@Test
-//	void shouldUpdateEmployee() {
-//		User func = new User(4, "nomeUpdated", "11111111111", "e@email.com", time);
-//		Optional<User> funcO= Optional.of(func);
-//		Mockito.when(userRepository.findById(func.getId())).thenReturn(funcO);
-//		Mockito.when(userRepository.save(func)).thenReturn(func);
-//		Optional<User> result = userController.updateUser(4, func);
-//		assertTrue(result.isPresent());
-//	} 
+	@Test()
+	void shouldNotRegisterUser() throws Exception {
+		
+		Mockito.when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+		
+		Exception exception = Assertions.assertThrows(Exception.class, () -> {
+			userController.registerUser(user);
+		});
+
+		Assertions.assertEquals("This element already exists", exception.getMessage());
+	}
+
+	@Test
+	void shouldUpdateEmployee() {
+		User usr1 = new User();
+		usr1.setUsername("user1");
+		Optional<User> user = Optional.of(usr1);
+
+		Mockito.when(userRepository.findByUsername(usr1.getUsername())).thenReturn(user);
+		Mockito.when(userRepository.save(usr1)).thenReturn(usr1);
+
+		Optional<User> result = userController.updateUser("user1", usr1);
+
+		assertTrue(result.isPresent());
+	}
 }
